51a143becdbfb195b2bf5c1903ae9cf198fe4dea
Masi <masumeebhami@gmail.com>
2025-06-04 22:56:01 +0200

    rework on gitignore file

--- target/debug/liblenient_derive.d
+++ target/debug/liblenient_derive.d
@@ -1,1 +1,1 @@
-/Users/masoumeh.ebhami-sefidehkhan/Desktop/lenient-codegen/target/debug/liblenient_derive.dylib: /Users/masoumeh.ebhami-sefidehkhan/Desktop/lenient-codegen/lenient_derive/src/lib.rs
+/Users/masoumeh/Desktop/lenient-codegen/target/debug/liblenient_derive.dylib: /Users/masoumeh/Desktop/lenient-codegen/lenient_derive/src/lib.rs
